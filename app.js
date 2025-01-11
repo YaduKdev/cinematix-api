05c76a626b68bb64c558c1da09108a9b3deeb250
Yadu Krishnan <79215964+YaduKdev@users.noreply.github.com>
2025-01-12 02:47:51 +0530

    App complete. Ready for deployment.

--- app.js
+++ app.js
@@ -20,8 +20,6 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", true);
 
-  console.log("Request received:", req.method, req.url);
-
   next();
 });
 
