44863e0027f1149fa765cf3867dffa63329c069e
Yadu Krishnan <79215964+YaduKdev@users.noreply.github.com>
2026-01-28 14:24:42 +0530

    remove cors

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ import userRouter from "./routes/user-routes.js";
 import adminRouter from "./routes/admin-routes.js";
 import movieRouter from "./routes/movie-routes.js";
 import bookingRouter from "./routes/booking-routes.js";
-import cors from "cors";
 
 dotenv.config();
 
@@ -15,23 +14,15 @@ const { PORT = 5000, MONGODB_PASSWORD, URL } = process.env;
 
 //middlewares
 app.use(express.json());
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", `${URL}`);
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   res.header("Access-Control-Allow-Credentials", true);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", `${URL}`);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", true);
 
-//   next();
-// });
-app.use(
-  cors({
-    origin: process.env.URL,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true,
-  })
-);
+  next();
+});
 
-app.use(express.json());
 //routes
 app.use("/user", userRouter);
 app.use("/admin", adminRouter);
